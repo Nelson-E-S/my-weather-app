9b7c1c25bcd5ca9d5ae44213835e8ae44f63d158
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-17 16:49:31 -0400

    added weather api to weathercard case

--- src/components/WeatherDisplayAllCards.js
+++ src/components/WeatherDisplayAllCards.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import WeatherCard from './WeatherCard';
+import axios from 'axios';
+import { useAsync } from 'react-async';
 
-class WeatherDisplayAllCards extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            parentData: [0,1,2]
-        }
-    }
-    render(){
+const loadWeatherZones = async () =>{
+    const res = await axios
+                        .get('https://api.weather.gov/zones/public')
+    if (!(res.status >= 200 && res.status < 300)) throw new Error(res.statusText);
+    return res.data;
+}
+
+const WeatherDisplayAllCards = ()=>{
+    const {data,error, isLoading} = useAsync({promiseFn: loadWeatherZones});
+    if(isLoading) return(
+        <div>
+        <h2><u>Weather Cards</u></h2>
+        <p>Loading zones...</p>
+    </div>
+    )
+    if(error){
+        console.log(error)
         return(
             <div>
                 <h2><u>Weather Cards</u></h2>
-                <div>
-                {
-                    this.state.parentData.map((item,index)=><WeatherCard parentData={item} key={index} index={index}/>)
-                }
-                </div>
+                <p>Error loading cards...</p>
+                <p>{error.message}</p>
             </div>
-        );
+        )
     }
+    //console.log(data.features.map((item,index)=>item.properties.id))
+    return(
+        <div>
+            <h2><u>Weather Cards</u></h2>
+            {data.features.map((item,index)=><WeatherCard key={index} parentData={item.properties.id} index={index} />)}
+        </div>
+    )
 }
 
 export default WeatherDisplayAllCards
