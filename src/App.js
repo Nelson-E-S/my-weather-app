f888de93c5c4741c056c9f1e1f0ce876452c5de4
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-17 15:35:37 -0400

    testing weather card case in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/App.css';
 
+//testing
+import AllCards from  './components/WeatherDisplayAllCards'
+
 function App() {
   return (
     <div className="App">
-
+      <AllCards />
     </div>
   );
 }
