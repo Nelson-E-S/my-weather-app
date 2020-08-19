db36b3f8704dd5949f0212d71426ec1506fce15b
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-18 23:22:48 -0400

    created userpage

---
+++ src/pages/UserPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import WeatherPersonalCards from '../components/WeatherPersonalCards';
+
+const UserPage = () =>{
+    return(
+        <div>
+            <WeatherPersonalCards />
+            <hr style={{height:'1px',width:'100%',background:'black'}} />
+        </div>
+    )
+}
